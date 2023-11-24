5ae399fc4757d90eeb3b8f2e464ec25b7c310e0b
lordzzz777 <114311106+lordzzz777@users.noreply.github.com>
2023-11-24 18:49:09 +0100

    Initial Commit

---
+++ CustonTextEditor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustonTextEditor
+//
+//  Created by Esteban Perez Castillejo on 24/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustonTextEditor/CustonTextEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustonTextEditorApp.swift
+//  CustonTextEditor
+//
+//  Created by Esteban Perez Castillejo on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CustonTextEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
